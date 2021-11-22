4ad8110df7f9203d25f303e6a538a51b941de991
Lj Miranda <ljvmiranda@gmail.com>
2021-11-22 11:01:40 +0800

    Implement Prodigy panel as webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-prodigy" is now active!');
+const defaultProdigyUrl = 'http://localhost:8080';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-prodigy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Prodigy!');
+
+export function activate(context: vscode.ExtensionContext) {
+
+	/**
+	 * Opens a VSCode panel with the Prodigy interface
+	 */
+	let startProdigyPanel = vscode.commands.registerCommand('vscode-prodigy.start', () => {
+		const panel = vscode.window.createWebviewPanel(
+			"prodigyPanel",
+			"Prodigy",
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				retainContextWhenHidden: true
+			}
+		);
+		panel.webview.html = getProdigyContent();
+		panel.iconPath = {
+			"dark": vscode.Uri.file("/assets/prodigy_dark.svg"),
+			"light": vscode.Uri.file("/assets/prodigy_light.svg")
+		};
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(startProdigyPanel);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+
+function getProdigyContent() {
+	return `
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Prodigy</title>
+	</head>
+	<body>
+		<iframe src="${defaultProdigyUrl}" width="100%" height="100%" style="position: absolute;"></iframe>
+	</body>
+	</html>`;
+}
